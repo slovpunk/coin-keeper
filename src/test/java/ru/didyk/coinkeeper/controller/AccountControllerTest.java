3be9a31f05cf2811680e735b67bbe0f49b7f3cff
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-03-21 20:32:46 +0300

    add some tests for account controller

---
+++ src/test/java/ru/didyk/coinkeeper/controller/AccountControllerTest.java
@@ -0,0 +1,94 @@
+package ru.didyk.coinkeeper.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.didyk.coinkeeper.model.Account;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(value = {"/account-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+class AccountControllerTest {
+
+    @Autowired
+    private AccountController accountController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(accountController).isNotNull();
+    }
+
+    @Test
+    void getAccount() throws Exception {
+        this.mockMvc.perform(get("/api/coinkeeper/ + {id}", 1L))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.balance", is(10000)));
+    }
+
+    @Test
+    void addAccount() throws Exception {
+        Account account = new Account();
+        account.setId(1L);
+        account.setBalance(9999L);
+
+        String requestJson = mapper.writeValueAsString(account);
+
+        mockMvc.perform(post(AccountController.PATH + "account")
+                        .content(requestJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)));
+    }
+
+    @Test
+    void updateAccount() throws Exception {
+        Account account = new Account();
+        account.setId(1L);
+        account.setBalance(9999L);
+
+        String requestJson = mapper.writeValueAsString(account);
+
+        mockMvc.perform(post(AccountController.PATH + "account")
+                        .content(requestJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)));
+    }
+
+    /*
+    TODO: не работает пока
+     */
+    @Test
+    void deleteAccount() throws Exception {
+
+        mockMvc.perform(delete(AccountController.PATH + "delete/" + "{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(2)));
+    }
+}
---
+++ src/test/resources/account-before.sql
@@ -0,0 +1,4 @@
+insert into account(id, balance)
+values(1, 10000);
+insert into account(id, balance)
+values(2, 2000);
